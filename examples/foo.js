81f8732e7614a65d94aa6ef3a585fe87995018f9
Anlv.Tan <anlv.tan@tendcloud.com>
2014-07-10 16:41:22 +0800

    example.

---
+++ examples/foo.js
@@ -0,0 +1,37 @@
+var _ = require('codash'),
+    co = require('co'),
+    LineReader = require('co-stream').LineReader,
+    Writer = require('co-stream').Writer,
+    WebHdfsClient = require('../');
+
+co(function *() {
+    var client = new WebHdfsClient({ namenode_host: '10.10.0.140' });
+
+    var home = yield* client.getHomeDirectory();
+    var files = yield* client.listStatus(home);
+    console.log(_.map(files, function (f) { return f.pathSuffix }));
+
+    yield* client.mkdirs(home + '/tmp');
+
+    var filename = home + '/tmp/foo.txt';
+
+    // Data style.
+    yield* client.create(filename, 'foooooo\n', { overwrite: true });
+    yield* client.append(filename, 'barr\n');
+    console.log(yield* client.open(filename));
+
+    // Stream style.
+    var stream = new Writer(yield* client.createWriteStream(filename, { overwrite: true }));
+    yield stream.writeline('xxxxxxx');
+    yield _.sleep(100);
+    yield stream.writeline('gggggg');
+    yield stream.end();
+    yield _.sleep(1000);
+
+    var reader = new LineReader(yield* client.createReadStream(filename));
+    for (var line; line = yield* reader.read();) {
+        console.log('line: ', line);
+    }
+})(function (err) {
+    console.log(err || 'finished');
+});
